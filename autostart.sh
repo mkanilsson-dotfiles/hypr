cd4acda4c87c41254ca06085d31488d7d824249f
Marcus Nilsson <marcus.nilsson@genarp.com>
2023-08-24 16:54:27 +0200

    Initial Commit

---
+++ autostart.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+WALLPAPER_PATH=$(~/Scripts/random_wallpaper.sh irl)
+
+echo "$WALLPAPER_PATH" > /tmp/current_wallpaper
+
+swaybg -i "$WALLPAPER_PATH" -o '*' &
+swaync &
+waybar &
+nm-applet &
